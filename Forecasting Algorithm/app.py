4c635f6818197c3adfbecdb915f83af8dcf98b57
Lacia7u7 <93607391+Lacia7u7@users.noreply.github.com>
2024-04-14 13:27:31 -0600

    Add files via upload

--- Forecasting Algorithm/app.py
+++ Forecasting Algorithm/app.py
@@ -2,8 +2,12 @@ import uuid
 from datetime import datetime
 
 import json
-from flask import jsonify, send_from_directory
 
+import numpy as np
+from flask import jsonify, send_from_directory
+from skforecast.model_selection import backtesting_forecaster
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from flask import Flask, request, render_template, redirect, url_for, flash
 from lightgbm import LGBMRegressor
 from werkzeug.utils import secure_filename
@@ -32,13 +36,15 @@ login_manager.init_app(app)
 
 
 #root folder
-ROOT_FOLDER = ".\\"
+ROOT_FOLDER = "E:\\Data\\UP\\8vo Semestre\\Big Data\\Forecasting Algorithm"
 # Define the folder where uploaded files will be stored
 UPLOAD_FOLDER = 'upload'
 STATIC_FOLDER = "static"
 DATASETS_FOLDER = "datasets"
 KEYS_FOLDER = "keys"
 MODELS_FOLDER = 'models'
+RESULTS_FOLDER = "results"
+FORECAST_FOLDER= "forecast"
 
 app.config['UPLOAD_FOLDER'] = os.path.join(ROOT_FOLDER, UPLOAD_FOLDER)
 app.config['STATIC_FOLDER'] = os.path.join(ROOT_FOLDER, STATIC_FOLDER)
@@ -173,13 +179,7 @@ def models():
     user_saved_models = []
     for model in user_models:
         model_data = model.to_dict()
-        user_saved_models.append({
-            'regressor': model_data['regressor'],
-            'params': model_data.get('params', None),  # Retrieve parameters if available
-            'upload_date':model_data.get('upload_date', None),
-            'file_url': model_data.get('file_url', None),  # Retrieve file URL if available
-            'train_params':model_data.get('train_params', None)
-        })
+        user_saved_models.append(model_data)
 
     # Pass user's saved models and regressors to the template
     return render_template('models.html', regressors=regressors, user_saved_models=user_saved_models)
@@ -197,13 +197,7 @@ def predict():
     user_saved_models = []
     for model in user_models:
         model_data = model.to_dict()
-        user_saved_models.append({
-            'regressor': model_data['regressor'],
-            'params': model_data.get('params', None),  # Retrieve parameters if available
-            'upload_date':model_data.get('upload_date', None),
-            'file_url': model_data.get('file_url', None),  # Retrieve file URL if available
-            'train_params':model_data.get('train_params', None)
-        })
+        user_saved_models.append(model_data)
 
     return render_template('predict.html', datasets=datasets, models=user_saved_models)
 
@@ -211,6 +205,7 @@ def predict():
 @login_required
 def save_model():
     regressor = request.form['regressor']
+    model_name = request.form['model_name']
 
     # Extract parameters for the chosen regressor from models.json
     with open(os.path.join(ROOT_FOLDER,MODELS_FOLDER,'models.json')) as f:
@@ -231,11 +226,28 @@ def save_model():
     model = create_model(regressor, model_params)
 
     file_id = str(uuid.uuid4())
-    model_id = file_id+"_"+regressor
+    model_id = model_name+"_"+regressor+"_"+file_id
 
     # Save the model as a file (you need to implement this function)
     model_file_url = save_model_as_file(model,username=current_user.username,model_id=model_id)
 
+    internal_save_model(model_file_url,model_name,model_id,regressor,model_params, get_empty_train_params())
+    os.remove(model_file_url)
+    return redirect(url_for('models'))
+
+def get_empty_train_params():
+    return {
+        'date_trained': None,
+        'dataset_root': None,
+        'temporal_column':None,
+        'y_column':None,
+        'group_by':None,
+        "clave":None,
+        'resampling_rule':None,
+        'steps':None
+    }
+
+def internal_save_model(model_file_url, model_name, model_id, regressor, model_params, train_params):
     blob = bucket.blob(UPLOAD_FOLDER+"/"+MODELS_FOLDER+"/"+current_user.username+"/"+model_id)
 
     blob.metadata = {"Content-Type": "application/octet-stream"}
@@ -246,29 +258,33 @@ def save_model():
 
     # Store model information in Firebase Firestore
     model_info = {
+        'root': model_id,
+        'name': model_name,
         'user': current_user.username,
         'regressor': regressor,
         'params': model_params,
         'file_url': file_url,  # Update with actual file path
         'upload_date': datetime.now(),
-        'train_params' : {
-            'date_trained': None,
-            'dataset_id': None,
-            'temporal_column':None,
-            'y_column':None,
-            'group_by':None,
-            'resampling_rule':None,
-            'steps':None
-        }
+        'train_params' : train_params
     }
     db.collection('models').document(current_user.username).collection('files').document(model_id).set(model_info)
-    os.remove(model_file_url)
     flash('Model saved successfully', 'success')
-    return redirect(url_for('models'))
 
 @app.route('/results')
 def results():
-    return render_template('results.html')
+    # Retrieve forecasts from Firestore
+    forecasts = db.collection(FORECAST_FOLDER).document(current_user.username).collection('files').get()
+
+    # Initialize an empty list to store forecast data
+    forecast_data = []
+
+    # Iterate over the forecast documents
+    for forecast in forecasts:
+        # Convert each forecast document to a dictionary and append it to the list
+        forecast_data.append(forecast.to_dict())
+
+    # Render the results.html template with forecast data
+    return render_template('results.html', forecasts=forecast_data)
 
 @app.route('/contact')
 def contact():
@@ -297,7 +313,14 @@ def upload_csv():
         # Get the username
         username = current_user.username
 
-        _secret_file_name = file_id + "_" + secure_filename(file.filename)
+        dataset_name_ = request.form['dataset_name']
+
+        # Extract the original filename without extension
+        filename_without_extension, original_extension = os.path.splitext(file.filename)
+
+        # Concatenate the parts to form the filename
+        _secret_file_name = f"{secure_filename(dataset_name_)}_{filename_without_extension}_{file_id}{original_extension}"
+
         subfolder_path = os.path.join(DATASETS_FOLDER, os.path.join(username, _secret_file_name))
 
         # Extract the directory path from the secret file name
@@ -328,6 +351,8 @@ def upload_csv():
 
         # Save dataset info to Firestore
         dataset_info = {
+            'root':_secret_file_name,
+            'name': dataset_name_,
             'user': username,
             'file_id': file_id,
             'original_name': secure_filename(file.filename),
@@ -337,7 +362,7 @@ def upload_csv():
             'file_url': file_url
             # Add more metadata if needed
         }
-        db.collection(DATASETS_FOLDER).document(username).collection('files').document(file_id).set(dataset_info)
+        db.collection(DATASETS_FOLDER).document(username).collection('files').document(_secret_file_name).set(dataset_info)
         flash('File uploaded successfully')
         return redirect(url_for('datasets'))
     else:
@@ -385,9 +410,14 @@ def upload():
 @app.route('/prepare_train_mapping', methods=['POST'])
 def prepare_train_mapping():
     # Get form data
-    model_url = request.form['model']
-    dataset_url = request.form['dataset']
+    model_root = request.form['model_root']
+    dataset_root = request.form['dataset_root']
 
+    model = db.collection(MODELS_FOLDER).document(current_user.username).collection('files').document(model_root).get().to_dict()
+    dataset = db.collection(DATASETS_FOLDER).document(current_user.username).collection('files').document(dataset_root).get().to_dict()
+
+    model_url = model['file_url']
+    dataset_url = dataset['file_url']
     # Generate a random folder ID
     random_folder_id = str(uuid.uuid4())
 
@@ -409,11 +439,30 @@ def prepare_train_mapping():
     os.remove(dataset_path)
     os.rmdir(folder_path)
     # Render the column mapping template
-    return render_template('train_model.html', columns=columns, model_url=model_url, dataset_url=dataset_url)
+    return render_template('train_model.html', columns=columns,dataset_root=dataset_root,model_root=model_root, model_url=model_url, dataset_url=dataset_url)
+
+def custom_metric(y_true, y_pred):
+    '''
+    Calculate the MAE, MSE, RMSE, and R2
+    '''
+    from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+    import numpy as np
+    mae = mean_absolute_error(y_true, y_pred)
+    mse = mean_squared_error(y_true, y_pred)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_true, y_pred)
+    return {"mae":mae, "mse":mse,"rmse":rmse, "r2":r2}
+
 
 @app.route('/train_model', methods=['POST'])
 def train_model():
     # Get form data
+    model_root = request.form['model_root']
+    dataset_root = request.form['dataset_root']
+
+    model_db = db.collection(MODELS_FOLDER).document(current_user.username).collection('files').document(model_root).get().to_dict()
+    dataset_db = db.collection(DATASETS_FOLDER).document(current_user.username).collection('files').document(dataset_root).get().to_dict()
+
     model_url = request.form['model_url']
     dataset_url = request.form['dataset_url']
 
@@ -421,7 +470,7 @@ def train_model():
     random_folder_id = str(uuid.uuid4())
 
     # Create a directory to store the files
-    folder_path = os.path.join('train', random_folder_id)
+    folder_path = os.path.join(ROOT_FOLDER,'train', random_folder_id)
     os.makedirs(folder_path, exist_ok=True)
 
     try:
@@ -478,6 +527,21 @@ def train_model():
         # Fit the forecaster to the resampled dataset
         forecaster.fit(y=df_resampled)
 
+        # Save the model to the file
+        with open(model_path, 'wb') as f:
+            pickle.dump(model, f)
+
+        internal_save_model(model_path,model_db['name'],model_db['root'],model_db['regressor'], model_db["params"],{
+            'date_trained': datetime.now(),
+            'dataset_root': dataset_db['root'],
+            'temporal_column': date_column,
+            'y_column': y_column,
+            'group_by': filter_column,
+            "clave":clave,
+            'resampling_rule':resampling_rule,
+            'steps':steps
+        })
+
         # Forecast future predictions
         forecast = forecaster.predict(steps=steps)  # Use user-specified steps
 
@@ -508,28 +572,90 @@ def train_model():
 
         # Create HTML file for the interactive plot
         plotly_output_filename = f'forecast_plot_{unique_id}.html'
-        plotly_output_path = os.path.join(app.config['STATIC_FOLDER'], plotly_output_filename)
+        plotly_output_path = os.path.join(ROOT_FOLDER,app.config['STATIC_FOLDER'], plotly_output_filename)
         fig.write_html(plotly_output_path)
 
+        # Upload the forecast results as a CSV file to the bucket
+        forecast_csv_filename = f'forecast_{unique_id}.csv'
+        forecast_csv_path = os.path.join(ROOT_FOLDER,app.config['STATIC_FOLDER'], forecast_csv_filename)
+        forecast.to_csv(forecast_csv_path, index_label='Date')
+
+        # Upload the CSV file to the bucket
+        blob = bucket.blob(f'{UPLOAD_FOLDER}/{FORECAST_FOLDER}/{current_user.username}/{forecast_csv_filename}')
+        blob.upload_from_filename(forecast_csv_path,predefined_acl='publicRead')
+        csv_url = blob.public_url
+
+        # Upload the HTML file to the bucket
+        blob = bucket.blob(f'{UPLOAD_FOLDER}/{FORECAST_FOLDER}/{current_user.username}/{plotly_output_filename}')
+        blob.upload_from_filename(plotly_output_path,predefined_acl='publicRead')
+        plot_url = blob.public_url
+
+        # Perform backtesting with the forecaster
+        metric, prediction_backtesting = backtesting_forecaster(
+            forecaster=forecaster,
+            y=df_resampled,  # Assuming 'Demand' is the target column
+            steps=steps,
+            metric=custom_metric,  # Use mean_absolute_error as the metric
+            initial_train_size=len(df_resampled[:forecast_start_date]),  # Use data up to forecast start date for training
+            refit=False,  # Do not refit the forecaster during backtesting
+            verbose=True,
+        )
+        # Store all relevant information in the database
+        forecast_root = f'forecast_{unique_id}'
+        forecast_data = {
+            'forecast_root': forecast_root,
+            'model_root': model_db["root"],
+            'dataset_root': dataset_db['root'],
+            'train_params': model_db["train_params"],
+            'datetime': datetime.now(),
+            'metrics': metric,
+            'csv_url': csv_url,
+            'plot_url': plot_url
+        }
+
+        db.collection(FORECAST_FOLDER).document(current_user.username).collection('files').document(forecast_root).set(forecast_data)
+
         # Delete temporary files and folder
+        os.remove(plotly_output_path)
+        os.remove(forecast_csv_path)
         os.remove(dataset_path)
         os.remove(model_path)
         os.rmdir(folder_path)
 
         # Redirect the user to the success page with the link to the interactive plot
-        return redirect(url_for('success', plotly_plot_url=plotly_output_path))
+        return redirect(url_for('success', forecast_root=forecast_root, csv_url=csv_url , plotly_plot_url=plot_url))
 
     except Exception as e:
         # Handle errors
         return render_template('error.html', error=str(e))
 
-@app.route('/plotly_plot/<path:filename>')
-def plotly_plot(filename):
-    return send_from_directory(app.config['STATIC_FOLDER'], filename)
+@app.route('/plotly_plot')
+def plotly_plot():
+    # Get the URL of the Plotly plot
+    plot_url = request.args.get("plot_url")
+
+    # Ensure the static folder exists
+    static_folder = app.config['STATIC_FOLDER']
+    os.makedirs(os.path.join(ROOT_FOLDER,static_folder), exist_ok=True)
+
+    # Download the plot from the URL
+    response = requests.get(plot_url)
+
+    # Save the plot to the static folder
+    filename = os.path.basename(plot_url)
+    with open(os.path.join(static_folder, filename), 'wb') as f:
+        f.write(response.content)
+
+    # Return the plot from the static folder
+    return send_from_directory(static_folder, filename)
 
 @app.route('/success')
 def success():
-    return render_template('success.html', plotly_plot_url=request.args["plotly_plot_url"])
+    return render_template('success.html',
+                           forecast_root=request.args["forecast_root"],
+                           csv_url=request.args["csv_url"],
+                           plotly_plot_url=request.args["plotly_plot_url"]
+                           )
 
 def create_model(regressor, model_params):
     if regressor == 'LinearRegression':
